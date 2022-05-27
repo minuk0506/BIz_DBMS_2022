3e10b40d478ed3cf943bb57c2dbe656f79130a6c
minuk0506 <leeminuk9238@daum.net>
2022-05-27 15:52:29 +0900

    0527

---
+++ 오라클/user3(2022-05-27).sql
@@ -0,0 +1,26 @@
+-- User3 화면
+
+DROP TABLE tbl_user;
+CREATE TABLE tbl_user(
+    username VARCHAR2(30) PRIMARY KEY,
+    password VARCHAR2(128) NOT NULL,
+    name nVARCHAR2(50),
+    email VARCHAR2(125), 
+    role VARCHAR2(10)
+);
+
+SELECT * FROM tbl_user;
+
+UPDATE tbl_student SET
+st_tel = '010-111-1111'
+WHERE st_num = '20220001';
+
+UPDATE tbl_student SET
+st_tel = '010-222-2222'
+WHERE st_num = '20220002';
+
+UPDATE tbl_student SET
+st_tel = '010-333-3333'
+WHERE st_num = '20220003';
+
+commit;
