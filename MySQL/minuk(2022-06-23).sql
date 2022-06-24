17c4905ee0a42dcd01cd55ba492328dc381a9fee
minuk0506 <leeminuk9238@daum.net>
2022-06-24 17:53:32 +0900

    0624

---
+++ MySQL/minuk(2022-06-23).sql
@@ -0,0 +1,11 @@
+use mydb;
+drop table tbl_ems;
+
+select * from tbl_ems;
+
+DESC tbl_users;
+describe tbl_users;
+
+show tables;
+
+SELECT * FROM tbl_users;
---
+++ MySQL/minuk(2022-06-24).sql
@@ -0,0 +1,26 @@
+create table tbl_memo(
+m_seq BIGINT primary KEY,
+m_author VARCHAR(25) NOT NULL,
+m_date VARCHAR(10) NOT NULL,
+m_time VARCHAR(10) NOT NULL,
+m_memo VARCHAR(40) NOT NULL,
+m_image VARCHAR(125)
+);
+
+SELECT * FROM tbl_memo;
+
+SELECT * FROM tbl_memo where m_seq;
+
+INSERT INTO tbl_memo(
+m_seq,
+m_author,
+m_date,
+m_time,
+m_memo
+) values(
+1,
+'농'.
+'2022',
+'12',
+'가나다'
+)
