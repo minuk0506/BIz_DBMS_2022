9901b527cb21ce49fe4af65735af7edb18de38c6
minuk0506 <leeminuk9238@daum.net>
2022-06-27 15:50:46 +0900

    0627

--- MySQL/minuk(2022-06-24).sql
+++ MySQL/minuk(2022-06-24).sql
@@ -1,11 +1,13 @@
 create table tbl_memo(
-m_seq BIGINT primary KEY,
+m_seq BIGINT primary KEY auto_increment,
 m_author VARCHAR(25) NOT NULL,
 m_date VARCHAR(10) NOT NULL,
 m_time VARCHAR(10) NOT NULL,
 m_memo VARCHAR(40) NOT NULL,
 m_image VARCHAR(125)
 );
+
+drop table tbl_memo;
 
 SELECT * FROM tbl_memo;
 
@@ -19,8 +21,9 @@ m_time,
 m_memo
 ) values(
 1,
-'농'.
+'농',
 '2022',
 '12',
 '가나다'
-)
+);
+SELECT seq_memo.NEXTVAL FROM DUAL;
